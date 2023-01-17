d72253c41ca7fc25958e6bcafc04c97b495e40b7
Midori <thaistsushima@gmail.com>
2023-01-17 19:22:44 -0300

    Edit js to display on the html the right text; edit html adding more camps

--- index.js
+++ index.js
@@ -6,73 +6,87 @@
     //if it`s a draw no one gets a point
 //there's 5 rounds and in the end it's announced the winner
 
-function capitalize(str) {
-    let cont = str.length;
-    let newStr;
-
-    for (var i = 0; i < cont; i++) {
-        if (i == 0) {
-            newStr = str[0].toUpperCase();
-        }
-        else {
-            newStr = newStr + str[i].toLowerCase();
-        }
-    }
-    return newStr;
-}
-
-function cleanPlayerSelection(playerSelection){
-    playerSelection = playerSelection.trim();
-    playerSelection = playerSelection.toLowerCase(); 
-    return capitalize(playerSelection);  
-}
-
-
 function getComputerChoice() {
     const options = ['Rock', 'Paper', 'Scissor'];
     return options[Math.floor(Math.random() * options.length)];
 }
-//console.log(getComputerChoice());
+
 
 function playRound(playerSelection, computerSelection){
 
+    //let round = document.getElementById("round");
+
     console.log('You choose:' + playerSelection);
     console.log('Computer choose:' + computerSelection);
+    numRound++;
+
+    document.getElementById("numRound").innerHTML = 'Round number ' + numRound;
     
     if (playerSelection === computerSelection) {
 
         console.log('It is a Draw!');
+        document.getElementById("round").innerHTML = 'You choose: ' + playerSelection +
+                                                     '<br>Computer choose: ' + computerSelection +
+                                                     '<br>It is a Draw!';
+
         return 0;
 
     } else if ((playerSelection == 'Rock' && computerSelection == 'Paper')      ||
                (playerSelection == 'Paper' && computerSelection == 'Scissor')   ||
                (playerSelection == 'Scissor' && computerSelection == 'Rock'))   {
         
-        console.log('You lost! '+ computerSelection +' beats '+ playerSelection);
+        document.getElementById("round").innerHTML = 'You choose: ' + playerSelection +
+                                                     '<br>Computer choose: ' + computerSelection +
+                                                     '<br>You lost! '+ computerSelection +' beats '+ playerSelection;        
+
         return 1;
 
     } else if ((playerSelection == 'Rock' && computerSelection == 'Scissor')    ||
                (playerSelection == 'Paper' && computerSelection == 'Rock')      ||
                (playerSelection == 'Scissor' && computerSelection == 'Paper'))  {
         
-        console.log('You won! '+ playerSelection +' beats '+ computerSelection);
+        document.getElementById("round").innerHTML = 'You choose: ' + playerSelection +
+                                                     '<br>Computer choose: ' + computerSelection +
+                                                     '<br>You won! '+ playerSelection +' beats '+ computerSelection;  
+
         return 2;
     }
     
 }
 
-//function game(){
+function keepScore (winner) {
+
+    if (winner == 1) {
+        cPoints++;
+    } else if (winner == 2 ) {
+        pPoints++;
+    }
+
+    document.getElementById("score").innerHTML = 'Your points: ' + pPoints +
+                                                 '<br> CPU points: ' + cPoints;
+
+    if (cPoints == 5){
+        console.log('Computer won the game!');
+        document.getElementById("result").innerHTML = 'Computer won the game!';
+        cPoints = 0;
+        pPoints = 0;
+    } else if (pPoints == 5) {
+        console.log('You won the game!');
+        document.getElementById("result").innerHTML = 'You won the game!';
+        cPoints = 0;
+        pPoints = 0;
+    }
+
+}
 
     let pPoints = 0;
     let cPoints = 0;
+    let numRound = 0;
     let winner;
 
     console.log('start');
 
     let playerSelection; // = cleanPlayerSelection(prompt("Choose between Rock, Paper and Scissor"));
-    //let computerSelection = getComputerChoice();
-
-    //console.log('computerSelection ' + getComputerChoice());
 
     document.getElementById('rock').addEventListener("click", function() {
         playerSelection = 'Rock';
@@ -90,45 +104,4 @@ function playRound(playerSelection, computerSelection){
         keepScore(winner);
     });
 
-    // if (winner == 1) {
-    //     cPoints++;
-    // } else if (winner == 2 ) {
-    //     pPoints++;
-    // }
-            
-    //console.log('Player points:' + pPoints + ' / Computer points:' + cPoints);            
-        
-    
 
-    // if (cPoints == 5) {
-    //     console.log('Computer won the game!');
-    // } else if (pPoints == 5) {
-    //     console.log('You won the game!');
-    // } else {
-    //     console.log('It is a draw! Both have ' + pPoints + ' points.');
-    // }
-
-//}
-
-//game();
-
-
-function keepScore (winner) {
-
-    if (winner == 1) {
-        cPoints++;
-    } else if (winner == 2 ) {
-        pPoints++;
-    }
-
-    if (cPoints == 5){
-        console.log('Computer won the game!');
-        cPoints = 0;
-        pPoints = 0;
-    } else if (pPoints == 5) {
-        console.log('You won the game!');
-        cPoints = 0;
-        pPoints = 0;
-    }
-
-}
